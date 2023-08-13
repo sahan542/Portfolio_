54a0c4598ddc19f91241ec2004477d828780873b
sahan542 <sahanrashmika542@gmail.com>
2023-08-13 22:36:25 +0530

    Other Components

--- script.js
+++ script.js
@@ -10,3 +10,34 @@ const menu = document.getElementById('menu');
 menu-icon.addEventListener('click', () => {
   menu.classList.toggle('show');
 });
+
+/*
+let menu = document.querySelector('#menu-icon');
+let navlist = document.querySelector('.navlist');
+*/
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navlist.classList.toggle('open');
+};
+window.onscroll = () => {
+    menu.classList.remove();
+    navlist.classList.remove('open');
+};
+
+
+/*Toggle Button*/
+// JavaScript function to toggle academic and curriculum content visibility
+function toggleContent(contentId) {
+    var content = document.getElementById(contentId);
+    if (content.style.display === 'none') {
+        content.style.display = 'block';
+    } else {
+  
+   
+   
+   
+   
+        content.style.display = 'none';
+    }
+  }
+
